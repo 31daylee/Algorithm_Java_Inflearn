556898f0bc45a33f849809064f6f5abea3c4a397
gussla2206 <gussla2206@gmail.com>
2024-01-03 06:18:36 +0900

    session 1 Test03

--- src/Ch01/Test3.java
+++ src/Ch01/Test3.java
@@ -10,8 +10,19 @@ public class Test3 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine(); // it is time to study
+        sc.close();
+        String answer = "";
+        int m = Integer.MIN_VALUE; // Integer에서 가장 작은 값으로 초기화 (=-2147483648)
+        String[] s = input.split(" "); // 공백 기준으로 분리하여 배열 리스트 생성
 
-
+        for(String x : s){
+            int len = x.length();
+            if(len > m){ // x의 길이가 최댓값보다 크다면
+                m = len; // 최댓값으로 할당
+                answer = x; // 해당하는 x를 answer 에 저장
+            }
+        }
+        System.out.println(answer);
     }
 }
