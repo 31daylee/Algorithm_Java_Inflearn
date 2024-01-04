725530f64955c5cb924a872dd7d2fcc2082019b5
gussla2206 <gussla2206@gmail.com>
2024-01-04 09:17:21 +0900

    session 07 회문 문자열 case 2 added

--- src/Ch01/Test7.java
+++ src/Ch01/Test7.java
@@ -5,19 +5,28 @@ import java.util.Scanner;
 /**
  * 날짜 : 2024/01/04
  * 내용 : 1-07 회문 문자열
+ * 방법 : Case1) charAt & toUpperCase 이용
+ *       Case2) StringBuilder reverse 이용
  */
 public class Test7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        str = str.toUpperCase();
+        // Case1 
+        /*str = str.toUpperCase();
         int len = str.length();
         String answer = "YES";
 
         for(int i=0; i<len/2; i++){
             if(str.charAt(i)!=str.charAt(len-1-i)) answer = "NO"; break;
 
-        }
+        }*/
+        
+        // Case2
+        String answer = "NO";
+        String tmp = new StringBuilder(str).reverse().toString();
+        if(str.equalsIgnoreCase(tmp)) answer = "YES";
+
         System.out.println(answer);
 
     }
